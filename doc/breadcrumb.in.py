95f148a5585eef8466c7e0b719c6f116cf92cda9
Reuben Thomas <rrt@sc3d.org>
2024-11-02 01:20:31 +0000

    Fix FIXME: apply special style to current item in breadcrumb

--- doc/breadcrumb.in.py
+++ doc/breadcrumb.in.py
@@ -12,15 +12,16 @@ page = sys.argv[1]
 # Extract page name and normalize path
 path = os.path.dirname(page)
 if path == ".":
-    path = "" # pylint: disable=invalid-name
+    path = ""
 parents = path.removesuffix("$")
 
 # Generate and print breadcrumb
 desc = os.path.basename(parents)
-tree = "" # pylint: disable=invalid-name
-while parents != "" and parents != "." and parents != "/":
-    # FIXME: Add class breadcrumb-active to first-produced (last) item
-    tree = f'<li class="breadcrumb-item"><a href="{BaseUrl}{parents}">{desc}</a></li>{tree}'
+tree = ""
+classes = "breadcrumb-item breadcrumb-active"
+while parents not in ("", ".", "/"):
+    tree = f'<li class="{classes}"><a href="{BaseUrl}{parents}">{desc}</a></li>{tree}'
+    classes = "breadcrumb-item"
     parents = os.path.dirname(parents)
     desc = os.path.basename(parents)
 print(f'<li class="breadcrumb-item"><a href="{BaseUrl}">$include{{Title.in.txt}}</a></li>{tree}')
