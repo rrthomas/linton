0238e694e0f450f9ac1c40138e9085eeab01f5b5
Reuben Thomas <rrt@sc3d.org>
2025-01-22 00:18:58 +0000

    Improve --version output (fix #8)

--- linton/__init__.py
+++ linton/__init__.py
@@ -18,16 +18,23 @@ def main(argv: list[str] = sys.argv[1:]) -> None:
     # Command-line arguments
     parser = argparse.ArgumentParser(
         description="Make a web site from Markdown files and other resources.",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
     )
     parser.add_argument(
         "-V",
         "--version",
         action="version",
-        version=f"%(prog)s {VERSION} by Reuben Thomas <rrt@sc3d.org>",
+        version=f"""%(prog)s {VERSION}
+© 2024 Reuben Thomas <rrt@sc3d.org>
+https://github.com/rrthomas/linton
+Distributed under the GNU General Public License version 3, or (at
+your option) any later version. There is no warranty.""",
     )
     warnings.showwarning = simple_warning(parser.prog)
 
-    subparsers = parser.add_subparsers(required=True, title="subcommands", metavar="SUBCOMMAND")
+    subparsers = parser.add_subparsers(
+        required=True, title="subcommands", metavar="SUBCOMMAND"
+    )
     publish.add_subparser(subparsers)
     serve.add_subparser(subparsers)
     init.add_subparser(subparsers)
