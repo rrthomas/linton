0fb8ac3bab87e6ffcc46f3c44929e07bdd279a8f
Alistair Turnbull <github@minworks.co.uk>
2025-04-08 15:38:52 +0100

    breadcrumb.in.py: use `pathlib.Path`

--- linton/init-pages/breadcrumb.in.py
+++ linton/init-pages/breadcrumb.in.py
@@ -1,35 +1,30 @@
 #!/usr/bin/env python3
 
 # /// script
-# requires-python = ">=3.9"
+# requires-python = ">=3.8"
 # ///
 
-import os.path
 import sys
 import urllib.parse
+from pathlib import Path
 
 
 # Get command-line arguments
-page = sys.argv[1]
+page = Path(sys.argv[1])
 
 # Extract page name and normalize path
-path = os.path.dirname(page)
-if path == ".":
-    path = ""
-parents = path.removesuffix("$")
+path = page.parent
 
 # Generate and print breadcrumb
-desc = os.path.basename(parents)
 tree = ""
 classes = "breadcrumb-item breadcrumb-active"
-while parents not in ("", ".", "/"):
-    quoted_parents = urllib.parse.quote(parents)
+while str(path) not in (".", "/"):
+    quoted_parents = urllib.parse.quote(str(path))
     tree = f'<li class="{classes}">' + \
-        f'<a href="$run(path-to-root.in.py,$path)/{quoted_parents}/index.html">{desc}</a>' + \
+        f'<a href="$run(path-to-root.in.py,$path)/{quoted_parents}/index.html">{path.name}</a>' + \
         f'</li>{tree}'
     classes = "breadcrumb-item"
-    parents = os.path.dirname(parents)
-    desc = os.path.basename(parents)
+    path = path.parent
 print(
     '<li class="breadcrumb-item">' + \
     '<a href="$run(path-to-root.in.py,$path)/index.html">$include(Title.in.txt)</a>' + \
